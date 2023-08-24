287e5d702617251a93fec1daf9516a0576c3e88f
M Fahrur Rizky <70459040+fahrurrizky@users.noreply.github.com>
2023-08-25 00:18:47 +0700

    slash

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const db = require("../models");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-app.use(cors({ origin: ["https://themajesticmixer.netlify.app/"] }));
+app.use(cors({ origin: ["https://themajesticmixer.netlify.app"] }));
 // app.use(
 //   cors({
 //     origin: [
@@ -27,15 +27,15 @@ app.use(express.json());
 // ===========================
 // NOTE : Add your routes here
 
-app.get("/api", (req, res) => {
-  res.send(`Hello, this is my API`);
-});
+// app.get("/api", (req, res) => {
+//   res.send(`Hello, this is my API`);
+// });
 
-app.get("/api/greetings", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello, Student !",
-  });
-});
+// app.get("/api/greetings", (req, res, next) => {
+//   res.status(200).json({
+//     message: "Hello, Student !",
+//   });
+// });
 
 const {
   userRouter,
@@ -55,34 +55,34 @@ app.use("/public", express.static(path.join(__dirname, "../public")));
 app.use("/api/public", express.static(path.resolve(__dirname,"../public")));
 
 // not found
-app.use((req, res, next) => {
-  if (req.path.includes("/api/")) {
-    res.status(404).send("Not found !");
-  } else {
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.path.includes("/api/")) {
+//     res.status(404).send("Not found !");
+//   } else {
+//     next();
+//   }
+// });
 
-// error
-app.use((err, req, res, next) => {
-  if (req.path.includes("/api/")) {
-    console.error("Error : ", err.stack);
-    res.status(500).send("Error !");
-  } else {
-    next();
-  }
-});
+// // error
+// app.use((err, req, res, next) => {
+//   if (req.path.includes("/api/")) {
+//     console.error("Error : ", err.stack);
+//     res.status(500).send("Error !");
+//   } else {
+//     next();
+//   }
+// });
 
-//#endregion
+// //#endregion
 
-//#region CLIENT
-const clientPath = "../../client/public";
-app.use(express.static(join(__dirname, clientPath)));
+// //#region CLIENT
+// const clientPath = "../../client/public";
+// app.use(express.static(join(__dirname, clientPath)));
 
-// Serve the HTML page
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, clientPath, "index.html"));
-});
+// // Serve the HTML page
+// app.get("*", (req, res) => {
+//   res.sendFile(join(__dirname, clientPath, "index.html"));
+// });
 
 //#endregion
 
