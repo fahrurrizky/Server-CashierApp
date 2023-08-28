38ec085d510c2b9dc338224ce8a352c8afea7994
M Fahrur Rizky <70459040+fahrurrizky@users.noreply.github.com>
2023-08-28 16:32:10 +0700

    gmail

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,7 @@ const { Op } = require("sequelize");
 
 
 const transporter = nodemailer.createTransport({
-    service: "hotmail",
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
